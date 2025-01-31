4d0c5396f0b84951c7f3308abf21159326f1e200
rohith-panasa <rohith.krishnan@panasatech.com>
2025-01-31 20:23:25 +0530

    Added URLS for snippet and tag

---
+++ snipboxapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import SnippetViewSet, TagViewSet
+
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet)
+router.register(r'tags', TagViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
