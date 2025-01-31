8d1598e592a5f2f9fdb47c5bcd6a401687721245
rohith-panasa <rohith.krishnan@panasatech.com>
2025-01-31 19:58:21 +0530

    Create models Tag and Snippet

--- snipboxapp/models.py
+++ snipboxapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag_title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.tag_title
+
+
+class Snippet(models.Model):
+    snippet_title = models.CharField(max_length=255)
+    note = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.snippet_title
